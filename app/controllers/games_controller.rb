55391171277f48d4e9d9a3201ab130dc516ddcf8
Jordane Celet <mrjocte@gmail.com>
2021-03-01 17:16:59 +0100

    Added: the base routes of the app

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,3 @@
+class GamesController < ApplicationController
+    skip_before_action :authenticate_user!, only: :index
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
+  resources :games do
+    resources :bookings, only: [:new, :create]
+  end
 
+  resources :bookings, only: [:destroy]
+
+  get "/mygames", to: "users#games"
+  get "/profile", to: "users#show"
+  get "/bookings", to: "users#index"
+
+  # TO DO LATER
+  # patch "/accept", to: "bookings#accept"
+  # patch "/decline", to: "bookings#decline"
 end
