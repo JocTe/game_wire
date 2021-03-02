dd5f539c3d1241f0804a9d810e49598ab6bdc133
Jordane Celet <mrjocte@gmail.com>
2021-03-02 19:43:25 +0100

    Added: columns to Games table, price:int duration:time number_of_player:integer

---
+++ db/migrate/20210302183839_add_columns_to_game.rb
@@ -0,0 +1,7 @@
+class AddColumnsToGame < ActiveRecord::Migration[6.0]
+  def change
+    add_column :games, :price, :integer
+    add_column :games, :duration, :time
+    add_column :games, :number_of_players, :integer
+  end
+end
