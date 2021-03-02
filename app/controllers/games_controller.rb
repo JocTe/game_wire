f4867a71d1ef809f43d020ef5582c3c395e7feee
Alexis Bouchiounane <alexis.bouchiouane@gmail.com>
2021-03-02 10:33:58 +0100

    add edit / update / destroy to games_controller + some basic CSS 4 Home

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,17 @@
 class GamesController < ApplicationController
     skip_before_action :authenticate_user!, only: :index
+
+  def edit
+  end
+
+  def update
+    @game.update(game_params)
+    redirect_to game_path(@game)
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to game_path(@game.user) # redirect to ?
+  end
 end
