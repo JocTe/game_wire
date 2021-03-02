242b7d6f33011b86b3d1c040196518757bf37efe
Alexis Bouchiounane <alexis.bouchiouane@gmail.com>
2021-03-02 12:06:54 +0100

    destroy fixed

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,27 @@
 class GamesController < ApplicationController
-    skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+
+  def index
+    @games = Game.all
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.user = current_user
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
+  end
 
   def edit
   end
@@ -12,6 +34,13 @@ class GamesController < ApplicationController
   def destroy
     @game = Game.find(params[:id])
     @game.destroy
-    redirect_to game_path(@game.user) # redirect to ?
+    redirect_to games_path # redirect to ?
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :description)
+    # params.require(:game).permit(:name, :description, photos: [])
   end
 end
