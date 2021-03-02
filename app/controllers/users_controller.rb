5a69b13e4f099e706c570bf415866ee2110c5e2b
Jordane Celet <mrjocte@gmail.com>
2021-03-02 22:08:00 +0100

    ADDED: the profile page, and modified path of user, games and booking controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   
+
   def index
     @bookings = current_user.bookings
   end
 
   def games
     @games = current_user.games
+  end
+
+  def show
+    @user = current_user
   end
 end
