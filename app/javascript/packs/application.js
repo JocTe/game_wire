a315f8011d24ff09ca4f99998974bbefbbd39ec4
Jordane Celet <mrjocte@gmail.com>
2021-03-03 16:17:00 +0100

    added turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,13 +8,18 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import { initFlatpickr } from "../plugins/flatpickr";
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+document.addEventListener('turbolinks:load', () => {
+    // Call your JS functions here
+    initFlatpickr();
+});
 
 
-import { initFlatpickr } from "../plugins/flatpickr";
-initFlatpickr();
