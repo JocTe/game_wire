c85e312854099e9d1cbe81b03f3dfc875b601b72
morgane quere <qu.morgane@gmail.com>
2021-03-04 14:37:28 +0100

    handle default picture

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -20,10 +20,7 @@ class GamesController < ApplicationController
   def create
     @game = Game.new(game_params)
     @game.user = current_user
-    authorize @game
-    if !@game.photo.attached?
-      default_image(@game)
-    end
+    authorize @game    
     if @game.save
       redirect_to game_path(@game)
     else
@@ -56,11 +53,6 @@ class GamesController < ApplicationController
 
   def game_params    
     params.require(:game).permit(:name, :description, :photo, :description, :number_of_players, :duration, :price)
-  end
-
-  def default_image(game)
-    file = URI.open('https://images.unsplash.com/photo-1578377375762-cbcc98d68af0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
-    game.photo.attach(io: file, filename: 'default.jpg', content_type: 'image/jpg')
   end
   
 end
