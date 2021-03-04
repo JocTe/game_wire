7318e8d45420335993903cdafa936d0f7e666220
Jordane Celet <mrjocte@gmail.com>
2021-03-04 16:18:55 +0100

    Clean the user controller and put profile in pages ctrl

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,2 @@
-class UsersController < ApplicationController
-  
-
-  # def index
-  #   @bookings = user.bookings
-  # end
-
-  # def games
-  #   @games = current_user.games
-  # end
-
-  def show
-    @user = current_user
-    @games = policy_scope(Game)
-    authorize @games
-    
-  end
+class UsersController < ApplicationController  
 end
