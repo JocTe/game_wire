9951f814297774b9da908580bb3169680d85760f
Alexis Bouchiounane <alexis.bouchiouane@gmail.com>
2021-03-04 12:36:47 +0100

    searchbar is ready, need some css later

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,12 +3,24 @@ require 'open-uri'
 class GamesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
   before_action :set_game, only: [ :show, :edit, :update, :destroy ]
+
   def index
-    @games = Game.all
     @games = policy_scope(Game)
+    @games = Game.all
+    if params[:query].present?
+      sql_query = " \
+        name ILIKE :query \
+        OR description ILIKE :query \
+        OR name @@ :query \
+        OR description @@ :query \
+        "
+      @games = Game.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @games = Game.all
+    end
   end
-  
-  def show    
+
+  def show
     @booking = Booking.new
   end
 
@@ -54,7 +66,7 @@ class GamesController < ApplicationController
     authorize @game
   end
 
-  def game_params    
+  def game_params
     params.require(:game).permit(:name, :description, :photo, :description, :number_of_players, :duration, :price)
   end
 
@@ -62,5 +74,5 @@ class GamesController < ApplicationController
     file = URI.open('https://images.unsplash.com/photo-1578377375762-cbcc98d68af0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
     game.photo.attach(io: file, filename: 'default.jpg', content_type: 'image/jpg')
   end
-  
+
 end
