06c86a2683d9448a2079c79cae150811d592a11e
Jordane Celet <mrjocte@gmail.com>
2021-03-05 14:17:29 +0100

    added validate on booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -19,7 +19,7 @@ class BookingsController < ApplicationController
     if @booking.save
       redirect_to profile_path
     else
-      render :new
+      redirect_to game_path(@game)
     end
   end
 
